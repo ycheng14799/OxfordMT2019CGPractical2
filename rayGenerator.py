1d74e36932cdec56d2ac3506dec64fc8b8234260
Yi Fei Cheng <ycheng2@swarthmore.edu>
2019-11-06 21:15:04 +0000

    Ray generation component appears complete.

--- rayGenerator.py
+++ rayGenerator.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class ray(object):
-    def __init__(self, origin, position):
+    def __init__(self, origin, direction):
         self.origin = origin
-        self.position = position
+        self.direction = direction
 
 class camera(object):
     def __init__(self, pos, to, upApprox,\
@@ -24,8 +24,44 @@ class camera(object):
         self.height = widthActual / aspect # Actual height of camera plane
         self.widthPix = widthPix # Number of pixels across width
         self.heightPix = int(widthPix / aspect) # Number of pixels across height
+        self.l = - (0.5 * self.width) # Left boundary of screen
+        self.r = 0.5 * self.width # Right boundary of screne
+        self.b = - (0.5 * self.height) # Bottom boundary of scene
+        self.t = 0.5 * self.height # Top boundary of scene
 
+    # Calculate orthographic ray through given (i,j) pixel
+    def calcPixelOrthoRay(self, i, j):
+        u = self.l + (self.r - self.l) * (i + 0.5) / self.widthPix
+        v = self.b + (self.t - self.b) * (j + 0.5) / self.heightPix
+        rayDirection = - self.w
+        rayOrigin = self.e + (u * self.u) + (v * self.v)
+        return ray(rayOrigin, rayDirection)
+
+    # Rays for orthographic projection
+    def calcOrthoRays(self):
+        rays = []
+        for i in range(0, self.widthPix):
+            for j in range(0, self.heightPix):
+                rays.append(self.calcPixelOrthoRay(i,j))
+        return rays
+
+    # Calculate perspective ray given (i,j) pixel
+    def calcPixelPerpsectiveRay(self, i, j):
+        u = self.l + (self.r - self.l) * (i + 0.5) / self.widthPix
+        v = self.b + (self.t - self.b) * (j + 0.5) / self.heightPix
+        rayOrigin = self.e
+        rayDirection = -(self.d * self.w) + (u * self.u) + (v * self.v)
+        rayDirection = rayDirection #/ np.linalg.norm(rayDirection)
+        return ray(rayOrigin, rayDirection)
+
+    # Rays for perspective projection
+    def calcPerspectiveRays(self):
+        rays = []
+        for i in range(0, self.widthPix):
+            for j in range(0, self.heightPix):
+                rays.append(self.calcPixelPerpsectiveRay(i,j))
+        return rays
 
 a = camera(pos = np.array([0.0,0.0,0.0]), to = np.array([0.0,1.0,0.0]),\
     upApprox = np.array([0.0,0.0,1.0]), focalLen = 1.0,\
-    aspect = 4.0/3.0, widthActual = 3.0, widthPix = 400)
+    aspect = 1.0, widthActual = 4.0, widthPix = 2)
