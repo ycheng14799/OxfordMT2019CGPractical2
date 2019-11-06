3c63046b6d8712d748be2b6d8a241b6221f70120
Yi Fei Cheng <ycheng2@swarthmore.edu>
2019-11-06 21:54:20 +0000

    TODO: Implement traingle object.

--- rayTracer.py
+++ rayTracer.py
@@ -2,14 +2,13 @@
 # The actual ray tracing program
 
 import numpy as np
-import rayGenerator
-import geometry
+from rayGenerator import Ray, Camera
+from geometry import Sphere
 
 def main():
     s = Sphere(np.array([0.0, 0.0, 0.0]), 1.0)
     ray = Ray(np.array([-2.0, 0.0, 0.0]), np.array([1.0, 0.0, 0.0]))
-    intersectInfo = ray.calcIntersection(ray)
-    print(intersectInfo[0])
+    intersectInfo = s.calcIntersection(ray)
 
 if __name__ == "__main__":
     main()
