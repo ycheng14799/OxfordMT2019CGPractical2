bfe2f9e1fa5412f3675f6fc4aef55608763ddeb6
Yi Fei Cheng <ycheng2@swarthmore.edu>
2019-11-07 11:49:05 +0000

    Left-right direction appears flipped. Bug in aspect ratio.

--- rayGenerator.py
+++ rayGenerator.py
@@ -27,7 +27,7 @@ class Camera(object):
         self.width = widthActual # Actual width of camera plane
         self.height = widthActual / aspect # Actual height of camera plane
         self.widthPix = widthPix # Number of pixels across width
-        self.heightPix = int(widthPix / aspect) # Number of pixels across height
+        self.heightPix = int(float(widthPix) / aspect) # Number of pixels across height
         self.l = - (0.5 * self.width) # Left boundary of screen
         self.r = 0.5 * self.width # Right boundary of screne
         self.b = - (0.5 * self.height) # Bottom boundary of scene
@@ -36,32 +36,33 @@ class Camera(object):
     # Calculate orthographic ray through given (i,j) pixel
     def calcPixelOrthoRay(self, i, j):
         u = self.l + (self.r - self.l) * (i + 0.5) / self.widthPix
-        v = self.b + (self.t - self.b) * (j + 0.5) / self.heightPix
-        rayDirection = - self.w
+        v = self.t - (self.t - self.b) * (j + 0.5) / self.heightPix
+        rayDirection = - self.w + 0.
+        rayDirection = rayDirection / np.linalg.norm(rayDirection)
         rayOrigin = self.e + (u * self.u) + (v * self.v)
-        return ray(rayOrigin, rayDirection)
+        return Ray(rayOrigin, rayDirection)
 
     # Rays for orthographic projection
     def calcOrthoRays(self):
         rays = []
-        for i in range(0, self.widthPix):
-            for j in range(0, self.heightPix):
+        for j in range(0, self.heightPix):
+            for i in range(0, self.widthPix):
                 rays.append(self.calcPixelOrthoRay(i,j))
         return rays
 
-    # Calculate perspective ray given (i,j) pixel
+    # Calculate perspective ray given (i,j) pixel (top left: (0,0))
     def calcPixelPerpsectiveRay(self, i, j):
         u = self.l + (self.r - self.l) * (i + 0.5) / self.widthPix
-        v = self.b + (self.t - self.b) * (j + 0.5) / self.heightPix
+        v = self.t - (self.t - self.b) * (j + 0.5) / self.heightPix
         rayOrigin = self.e
         rayDirection = -(self.d * self.w) + (u * self.u) + (v * self.v)
         rayDirection = rayDirection / np.linalg.norm(rayDirection)
-        return ray(rayOrigin, rayDirection)
+        return Ray(rayOrigin, rayDirection)
 
     # Rays for perspective projection
     def calcPerspectiveRays(self):
         rays = []
-        for i in range(0, self.widthPix):
-            for j in range(0, self.heightPix):
+        for j in range(0, self.heightPix):
+            for i in range(0, self.widthPix):
                 rays.append(self.calcPixelPerpsectiveRay(i,j))
         return rays
--- rayTracer.py
+++ rayTracer.py
@@ -2,11 +2,35 @@
 # The actual ray tracing program
 
 import numpy as np
-from rayGenerator import Ray, Camera
-from geometry import Sphere
+from rayGenerator import Camera, Ray
+from geometry import Sphere, Triangle
 
-def main():
-    
+# Scene definition
+scene = []
+scene.append(Sphere(np.array([0.0,2.0,0.0]),\
+    2.0, np.array([0.0, 0.0, 1.0])))
+scene.append(Triangle(np.array([0.0, 0.0, 0.0]), np.array([0.0, -4.0, 0.0]),\
+    np.array([0.0, -4.0, 4.0]), np.array([0.0, 0.0, 0.0])))
 
-if __name__ == "__main__":
-    main()
+# Ray generation
+cam = Camera(np.array([-10.0, 0.0, 0.0]),\
+    np.array([1.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0]),\
+    10.0, 1.0, 10.0, 5)
+viewRays = cam.calcOrthoRays()
+
+# Ray intersection
+screenWidth = cam.widthPix
+screenHeight = cam.heightPix
+screen = np.zeros((screenHeight, screenWidth))
+
+for i in range(0, screenHeight):
+    for j in range(0, screenWidth):
+        for obj in scene:
+            intersectInfo = obj.calcIntersection(viewRays[(i * screenHeight) + j])
+            print(intersectInfo[0])
+            print(j,i)
+            print(viewRays[(i * screenHeight) + j].origin)
+            if(intersectInfo[0] == True):
+                screen[i,j] = 1.0
+
+print(screen)
