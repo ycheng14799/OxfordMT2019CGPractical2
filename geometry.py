b61a43c6744431e6690a5ffacfa3d48501ef1832
Yi Fei Cheng <ycheng2@swarthmore.edu>
2019-11-07 10:16:12 +0000

    Triangle implemented.

--- geometry.py
+++ geometry.py
@@ -2,12 +2,18 @@
 # Definitions for geometry (sphere, triangle)
 
 import numpy as np
-import rayGenerator
+from rayGenerator import Ray
 
 class Sphere(object):
-    def __init__(self, center, radius):
-        self.c = center # Sphere center
-        self.R = radius # Sphere radius
+    def __init__(self, center, radius, color):
+        self.c = center + 0. # Sphere center
+        self.R = radius + 0. # Sphere radius
+        self.color = color + 0. # Sphere color
+
+    # Method for calculating normals
+    def calcNormal(self, p):
+        n = (p - self.c) / self.R
+        return n
 
     # Method for calculating intersections
     def calcIntersection(self, ray):
@@ -16,10 +22,49 @@ class Sphere(object):
             - np.dot(ray.direction, ray.direction) * \
             (np.dot(ray.origin - self.c, ray.origin - self.c) - self.R**2)
         if discrim < 0:
-            return (False, np.full((3,),np.inf))
+            return (False, np.inf, np.inf)
         else:
             t1 = (- np.dot(ray.direction, ray.origin - self.c) + discrim)\
                 / np.dot(ray.direction, ray.direction)
             t2 = (- np.dot(ray.direction, ray.origin - self.c) - discrim)\
                 / np.dot(ray.direction, ray.direction)
             return (True, t1, t2)
+
+class Triangle(object):
+    def __init__(self, a, b, c, color): # Triangle with vertices a, b, c
+        self.a = a + 0.
+        self.b = b + 0.
+        self.c = c + 0.
+        self.color = color + 0. # Triangle color
+
+    # Method for calculating intersections
+    def calcIntersection(self, ray):
+        # Write out systems of equations
+        sysA = np.array([self.a - self.b, self.a - self.c, ray.direction]).T
+        sysb = (self.a - ray.origin).T
+
+        # Calculate M
+        M = np.linalg.det(sysA)
+        if M == 0:
+            return (False, np.inf)
+
+        # Calculate t
+        tMat = np.copy(sysA)
+        tMat[:,2] = sysb
+        t = np.linalg.det(tMat) / M
+
+        # Calculate gamma:
+        gammaMat = np.copy(sysA)
+        gammaMat[:,1] = sysb
+        gamma = np.linalg.det(gammaMat) / M
+        if (gamma < 0) or (gamma > 1):
+            return (False, np.inf)
+
+        # Calculate beta:
+        betaMat = np.copy(sysA)
+        betaMat[:,0] = sysb
+        beta = np.linalg.det(betaMat) / M
+        if (beta < 0) or (beta > (1 - gamma)):
+            return (False, np.inf)
+
+        return (True, t)
--- rayTracer.py
+++ rayTracer.py
@@ -6,9 +6,7 @@ from rayGenerator import Ray, Camera
 from geometry import Sphere
 
 def main():
-    s = Sphere(np.array([0.0, 0.0, 0.0]), 1.0)
-    ray = Ray(np.array([-2.0, 0.0, 0.0]), np.array([1.0, 0.0, 0.0]))
-    intersectInfo = s.calcIntersection(ray)
+    
 
 if __name__ == "__main__":
     main()
