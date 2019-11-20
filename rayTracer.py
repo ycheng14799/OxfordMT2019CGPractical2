2845d921e3a6f48ed70431aceb39fb153f7afc62
Yi Fei Cheng <ycheng2@swarthmore.edu>
2019-11-20 19:37:48 +0000

    Skybox implemented. Beginning implementation of reflection and refraction.

--- rayTracer.py
+++ rayTracer.py
@@ -2,7 +2,6 @@
 # The actual ray tracing program
 
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 from rayGenerator import Camera, Ray
 from geometry import Sphere, Triangle
@@ -13,6 +12,32 @@ COLOR_CHANNELS = 3
 def calcIntersectPoint(camPos, ray, t):
     rayDir = ray.direction / np.linalg.norm(ray.direction)
     return camPos + t * rayDir
+
+def rayColor(cam, scene, lights, j, i):
+    ray = cam.calcPixelPerpsectiveRay(j,i)
+    intersected = False
+    intersectT = np.inf
+    intersectObj = scene[0]
+    for obj in scene:
+        intersectInfo = obj.calcIntersection(ray)
+        if intersectInfo[0] == True:
+            minIntersect = min(intersectInfo[1:])
+            if minIntersect < intersectT and minIntersect > 0:
+                intersectT = minIntersect
+                intersected = True
+                intersectObj = obj
+    if intersected == True:
+        intersectPoint = calcIntersectPoint(cam.e, ray, intersectT)
+        # Calculate intersection normal
+        intersectionNormal = intersectObj.calcNormal(intersectPoint)
+        # temporarily define a lighting position
+        pointShade = 0.0
+        for light in lights:
+            pointShade += blinnPhongShadePoint(scene, light.intensity,\
+                intersectObj.color, intersectObj.ka, intersectObj.kd,\
+                intersectObj.ks, intersectObj.p, intersectionNormal,\
+                intersectPoint, cam.e, light.position)
+        screen[i,j] = pointShade
 
 def inShadow(scene, pointPos, lightPos, startOffset):
     # Calculate light vector
@@ -26,7 +51,6 @@ def inShadow(scene, pointPos, lightPos, startOffset):
             if intersection[1] > 0 and intersection[2] > 0:
                 return True
     return False
-
 
 def blinnPhongShadePoint(scene, I, color, ka, kd, ks, p, n, pointPos, camPos, lightPos):
     # I: Light intensity
@@ -66,21 +90,28 @@ scene.append(Triangle(np.array([-2.0,-2.0,-1.0]),\
     np.array([2.0,2.0,-1.0]),np.array([2.0,-2.0,-1.0]),\
     np.array([1.0,1.0,1.0]), 0.25, 0.5, 0.5, 1))
 scene.append(Sphere(np.array([0.0,0.0,0.0]),\
-    1.0, np.array([1.0, 1.0, 1.0]), 0.25, 0.5, 0.5, 32))
-
+    1.0, np.array([1.0, 0.5, 0.5]), 0.25, 0.5, 0.5, 32))
 
 # Lights
 lights = []
 lights.append(PointLight(np.array([0.0, 3.0, 3.0]),\
-    np.array([0.75, 0.5, 0.5])))
+    np.array([1.0, 1.0, 1.0])))
 lights.append(PointLight(np.array([0.0, -3.0, 3.0]),\
-    np.array([0.5, 0.5, 0.75])))
+    np.array([1.0, 1.0, 1.0])))
 
-# Ray generation
+# Sphere map
+map = cv2.imread("resources/autumnCubeMap.hdr", -1)
+print(map.shape)
+print(np.amin(map))
+print(np.amax(map))
+tonemapReinhard = cv2.createTonemapReinhard(1.5, 1.0,0.0,1.0)
+map = tonemapReinhard.process(map)
+mapHeight = map.shape[1]
+mapWidth = map.shape[0]
+
 cam = Camera(np.array([-5.0, 0.0, 0.0]),\
-    np.array([1.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0]),\
-    2.0, 1.0, 4.0, 500)
-viewRays = cam.calcPerspectiveRays()
+    np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0]),\
+    20, 1.25, 25, 1000)
 
 # Ray intersection
 screenWidth = cam.widthPix
@@ -89,12 +120,12 @@ screen = np.zeros((screenHeight, screenWidth, COLOR_CHANNELS))
 
 for i in range(0, screenHeight):
     for j in range(0, screenWidth):
-        screenPos = (i * screenWidth) + j
+        ray = cam.calcPixelPerpsectiveRay(j,i)
         intersected = False
         intersectT = np.inf
         intersectObj = scene[0]
         for obj in scene:
-            intersectInfo = obj.calcIntersection(viewRays[screenPos])
+            intersectInfo = obj.calcIntersection(ray)
             if intersectInfo[0] == True:
                 minIntersect = min(intersectInfo[1:])
                 if minIntersect < intersectT and minIntersect > 0:
@@ -102,7 +133,7 @@ for i in range(0, screenHeight):
                     intersected = True
                     intersectObj = obj
         if intersected == True:
-            intersectPoint = calcIntersectPoint(cam.e, viewRays[screenPos], intersectT)
+            intersectPoint = calcIntersectPoint(cam.e, ray, intersectT)
             # Calculate intersection normal
             intersectionNormal = intersectObj.calcNormal(intersectPoint)
             # temporarily define a lighting position
@@ -113,13 +144,18 @@ for i in range(0, screenHeight):
                     intersectObj.ks, intersectObj.p, intersectionNormal,\
                     intersectPoint, cam.e, light.position)
             screen[i,j] = pointShade
-
+        else:
+            dir = ray.direction
+            phi = (np.pi + np.arctan2(dir[1],dir[0])) / (2 * np.pi)
+            theta = (-np.pi + np.arccos(dir[2])) / np.pi
+            screen[i,j] = map[int(theta * mapWidth), int(phi * mapHeight)]
+# Prepare screen
+pixMax = np.amax(screen)
+screen[:,:,0] = screen[:,:,0] / pixMax
+screen[:,:,1] = screen[:,:,1] / pixMax
+screen[:,:,2] = screen[:,:,2] / pixMax
 
 # Display screen
 cv2.imshow('rendered', screen)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# Shading
-
-print(screen)
